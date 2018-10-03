5ef865d80171c4843b081bb81775860dd9167466
Vinicius Serpa <vinicius.serpa@gmail.com>
2018-10-03 20:08:47 -0300

    show product list with FlatList

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import api from '../services/api';
 
 export default class Main extends Component {
@@ -20,15 +20,26 @@ export default class Main extends Component {
         const response = await api.get("/products");
         const { docs } = response.data;
         this.setState({ docs });
-    }
+    };
+
+    renderItem = ({ item }) => (
+        <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+            <TouchableOpacity inPress={() => {}}>
+                <Text>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+    );
 
     render() {
         return (
             <View>
-                <Text>Main Page:</Text>
-                {this.state.docs.map(product => {
-                    return <Text key={product._id}>{product.title}</Text>;
-                })}
+                <FlatList
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
             </View>
         );
     }
