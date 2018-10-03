0f267f1932abff0ed38d286d40e4ca5eae40bb72
Vinicius Serpa <vinicius.serpa@gmail.com>
2018-10-02 22:30:03 -0300

    header with title and style

---
+++ hunt/src/routes.js
@@ -0,0 +1,13 @@
+import { createStackNavigator } from 'react-navigation';
+import Main from './pages/main';
+
+export default createStackNavigator({
+    Main
+}, {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#DA552F"
+        },
+        headerTintColor: "#FFF"
+    }
+});
