4bb7b0e5ca522feb73dc72e896bc5693ceebd497
Vinicius Serpa <vinicius.serpa@gmail.com>
2018-10-02 22:50:28 -0300

    access an api

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
 
@@ -7,10 +8,27 @@ export default class Main extends Component {
         title: "JSHunt"
     };
 
+    state = {
+        docs: []
+    };
+
+    componentDidMount() {
+        this.loadProducts();
+    };
+
+    loadProducts = async() => {
+        const response = await api.get("/products");
+        const { docs } = response.data;
+        this.setState({ docs });
+    }
+
     render() {
         return (
             <View>
-                <Text>Main Page</Text>
+                <Text>Main Page:</Text>
+                {this.state.docs.map(product => {
+                    return <Text key={product._id}>{product.title}</Text>;
+                })}
             </View>
         );
     }
