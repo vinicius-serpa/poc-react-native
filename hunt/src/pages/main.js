ad596ae25d9d9568ed91475309e3785aa109b64c
Vinicius Serpa <vinicius.serpa@gmail.com>
2018-10-03 21:07:37 -0300

    create webview for product url

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -32,7 +32,11 @@ export default class Main extends Component {
         <View style={styles.productContainer}>
             <Text style={styles.productTitle}>{item.title}</Text>
             <Text style={styles.productDescription}>{item.description}</Text>
-            <TouchableOpacity style={styles.productButton} inPress={() => {}}>
+            <TouchableOpacity 
+                style={styles.productButton} 
+                onPress={() => {
+                    this.props.navigation.navigate("Product", { product: item });
+                }}>
                 <Text style={styles.productButtonText}>Acessar</Text>
             </TouchableOpacity>
         </View>
